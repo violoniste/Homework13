b39b7de1ab51c94fbd4c0f4a85e5ecc44796eed6
BlackIron <violoniste2@gmail.com>
2023-08-18 22:59:22 +0700

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author lev = new Author("Лев", "Толстой");
+        Author aleksey = new Author("Алексей", "Толстой");
+
+        Book warAndPeace = new Book("Война и мир", lev, 1865);
+        Book aelita = new Book("Аэлита", aleksey, 1923);
+
+        warAndPeace.setYear(1869);
+
+        System.out.println(aelita);
     }
 }
